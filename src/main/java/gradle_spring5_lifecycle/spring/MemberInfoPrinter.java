052edbb916b2fb441d73695c49dc3e6fbadb90f9
lenovo <lenovo@DESKTOP-CIIGDBG>
2020-09-24 12:31:27 +0900

    sdsd

--- src/main/java/gradle_spring5_lifecycle/spring/MemberInfoPrinter.java
+++ src/main/java/gradle_spring5_lifecycle/spring/MemberInfoPrinter.java
@@ -1,7 +1,6 @@
 package gradle_spring5_lifecycle.spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
